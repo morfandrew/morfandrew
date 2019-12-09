e4ef092be726a4e7862002f1cb2c645ca76fab27
morfandrew <58331316+morfandrew@users.noreply.github.com>
2019-12-09 09:58:40 +0500

    Добавил папку Code
    Урок 2

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-debugger;
+const list = [
+    {id: 1, name: 'Документ 1'},
+    {id: 2, name: 'Документ 2'},
+    {id: 3, name: 'Документ 3'}
+];
+
+const $tbody = document.querySelector('table tbody');
+$tbody.innerHTML = '';
+for (let index = 0; index < list.length; index++) {
+    const element = list[index];
+    const $tr = document.createElement('tr');
+    const $tdId = document.createElement('td');
+    const $tdNumber = document.createElement('td');
+    $tdId.innerHTML = element.id;
+    $tdNumber.innerHTML = element.name;
+    $tr.append($tdId, $tdNumber);
+    $tbody.appendChild($tr);
+}
+
+const $searchButton = document.querySelector('.filter button');
+const $filterInput = document.querySelector('.filter input');
+const filterClickHandler = function (e) {
+
+    const q = $filterInput.value;
+    $tbody.innerHTML = '';
+    for (let index = 0; index < list.length; index++) {
+        const element = list[index];
+
+        if (element.name.indexOf(q) > -1) {
+            const $tr = document.createElement('tr');
+            const $tdId = document.createElement('td');
+            const $tdNumber = document.createElement('td');
+            $tdId.innerHTML = element.id;
+            $tdNumber.innerHTML = element.name;
+            $tr.append($tdId, $tdNumber);
+            $tbody.appendChild($tr);
+        }
+    }
+};
+$searchButton.addEventListener('click', filterClickHandler);
